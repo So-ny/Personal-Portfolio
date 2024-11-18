cc9fd4bd804008bc15a9c5745c725b40fdea51c8
So-ny <syonchau@gmail.com>
2024-11-17 20:48:03 -0800

    Added gradient background to image, and made it so it was round and no overflow :)

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -45,7 +45,8 @@ const Hero = () => {
             
 
             <div className="hidden lg:block">
-                <figure className="">
+                <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25%
+                via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
                     <img 
                     src="/images/hero-banner.png"
                     width = {656}
