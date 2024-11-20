5b7712826fb7ea23febd6cd0fefdb8ee4931d3c6
So-ny <syonchau@gmail.com>
2024-11-20 13:09:43 -0800

    Finished Work section

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -54,7 +54,7 @@ const Work = () => {
             <h2 className="headline-2 mb-8">
                 My portfolio higlhights
             </h2>
-            <div className="">
+            <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
                 {works.map(({ imgSrc, title, tags, projectLink },key)=>(
                     <ProjectCard 
                         key={key}
